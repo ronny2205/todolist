d129e976b100e388ff9137595c53b0d96b634847
Ronny Almog <ronny2205@gmail.com>
2016-09-23 16:04:12 +0000

    writing a test for listing users

---
+++ test/controllers/user_controller_test.exs
@@ -0,0 +1,12 @@
+defmodule ListingUsers.UserControllerTest do
+  use ExUnit.Case, async: true
+  use Plug.Test
+  alias Todolist.Router
+
+  @opts Router.init([])
+  test 'listing users' do
+    conn = conn(:get, "/users")
+    response = Router.call(conn, @opts)
+    assert response.status == 200
+  end
+end
