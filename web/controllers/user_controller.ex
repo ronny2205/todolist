09626edc3e972002929b4b5abf89e28f0682ac44
Ronny Almog <ronny2205@gmail.com>
2016-09-24 03:54:07 +0000

    reading the data.csv file

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -4,7 +4,27 @@ defmodule Todolist.UserController do
   
 
   def index(conn, _params) do
-    users1 = %{name: "Yafa", task: "Eat brunch"}
+  
+    {status, body} = File.read("web/static/assets/data.csv")
+    if status == :ok do
+        lines = String.split body, "\n"
+        users1 = Enum.map(lines, fn(line) -> split_line(line) end)
+        # remove the file title
+        users1 = List.delete_at(users1, 0)
+        users1 = Enum.uniq(users1)
+    else
+        IO.puts "error reading: web/static/assets/data.csv"
+    end
+  
+    # users1 = CSVLixir.read("web/static/assets/data.csv") |> Enum.to_list
+  
+    # users1 = %{name: "Yafa", task: "Eat brunch"}
     render conn, users: users1
+  end
+  
+  def split_line(line) do
+    [name, task, date] = String.split line, "|"
+    %{name: name}
+    # %{name: name, task: task, date: date}
   end
 end
